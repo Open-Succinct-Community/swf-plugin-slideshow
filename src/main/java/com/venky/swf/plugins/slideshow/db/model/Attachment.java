c353dfe6ae6124b6850ffb4fa305ad5eeefee8b1
Venky <venkatramanm@gmail.com>
2021-06-09 22:23:17 +0530

    uk issue nullable column

--- src/main/java/com/venky/swf/plugins/slideshow/db/model/Attachment.java
+++ src/main/java/com/venky/swf/plugins/slideshow/db/model/Attachment.java
@@ -3,7 +3,7 @@ package com.venky.swf.plugins.slideshow.db.model;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 
 public interface Attachment extends com.venky.swf.plugins.attachment.db.model.Attachment {
-    @UNIQUE_KEY
+    @UNIQUE_KEY(allowMultipleRecordsWithNull = false)
     public Long getSlideId();
     public void setSlideId(Long id);
     public Slide getSlide();
